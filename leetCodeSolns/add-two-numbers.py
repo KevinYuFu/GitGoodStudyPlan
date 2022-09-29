2d031a4f13001ba7decf75eece831038bac7a7dc
Yu Fu <kevin.yu.fu@gmail.com>
2022-09-29 19:08:06 -0400

    Add hint for add two number

---
+++ leetCodeSolns/add-two-numbers.py
@@ -0,0 +1,54 @@
+# 2. Add Two Numbers
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Convert a Num into ListList with while loop
+def numToList1(num: int) -> Optional[ListNode]:
+    listNodeNum = ListNode()
+
+    iterator = listNodeNum
+    while True:
+        iterator.next = ListNode(num % 10)
+
+        if num < 10:
+            break
+
+        num = int(num / 10)
+        iterator = iterator.next
+
+    return listNodeNum.next
+
+
+# Convert a Num into ListList with recursion
+def numToList2(num: int) -> Optional[ListNode]:
+    if num < 10:
+        return ListNode(num)
+
+    newNode = ListNode(num % 10)
+    newNode.next = numToList2(int(num/10))
+    return newNode
+
+    # Faster:
+    #return ListNode(num%10, numToList2(int(num/10)))
+
+# Print List
+def printLinkedList(listNode):
+    if listNode:
+        print(listNode.val)
+        printLinkedList(listNode.next)
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        pass
+
+
+printLinkedList(numToList1(123))
+print("_____")
+printLinkedList(numToList2(123))
